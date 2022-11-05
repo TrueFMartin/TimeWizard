f4ef18c172fd22029ace4e8086e5045a779ba22d
TrueFMartin <103139765+TrueFMartin@users.noreply.github.com>
2022-11-05 02:29:31 -0500

    No longer working now with intro of Jar
    Will fix later

--- buildLinuxMac.sh
+++ buildLinuxMac.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 #You may have to change your bash path from what is listed above
-cd src
-javac main/*.java -d ./bin
-java -cp ./bin main.Game  
+#cd src
+#javac main/*.java -d ./bin
+#java -cp ./bin main.Game  
+#NOTWORKING, now with Jar format I need to fix this
